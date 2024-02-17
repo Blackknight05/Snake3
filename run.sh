7f2357c16d367350b86db073fe38fdb7aabb0ed7
Blackknight05 <149324803+Blackknight05@users.noreply.github.com>
2024-02-16 23:39:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y python3-pip xvfb
 
 # Install Python dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Set up virtual display with Xvfb
 export SDL_VIDEODRIVER=x11
